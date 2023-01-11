ae3129d18fa4c0ff43ebf069e134bf624cdb1566
pierre <garnierpascalweb@gmail.com>
2023-01-11 21:11:51 +0100

    2.0.0: ajout de bootstrap 3..7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ShowComponent } from './show/show.component';
 import { MarkComponent } from './mark/mark.component';
@@ -9,7 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes : Routes = [
   { path : 'show', component : ShowComponent },
-  { path : 'mark', component : MarkComponent }  
+  { path : 'mark', component : MarkComponent }
 ] ;
 
 @NgModule({
@@ -19,8 +19,7 @@ const appRoutes : Routes = [
     MarkComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    BrowserModule,    
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
