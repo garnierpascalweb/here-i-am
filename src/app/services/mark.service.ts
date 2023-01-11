1b24a9432be93237da16f3ff82c1ddbb20724134
pierre <garnierpascalweb@gmail.com>
2023-01-11 21:22:44 +0100

    2.0.0: mark: MarkService

---
+++ src/app/services/mark.service.ts
@@ -0,0 +1,19 @@
+
+/**
+ * Service Mark
+ * @since 2.0.0
+ */
+export class MarkService {
+    
+    constructor(){
+
+    }
+
+    /**
+     * Marque la position de l'utilisateur et envoi au backend
+     * @since 2.0.0
+     */
+    markPosition(){
+
+    }
+}
