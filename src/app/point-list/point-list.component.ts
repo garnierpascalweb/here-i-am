d6090493d955eb0337d65996653ad2995ae139a3
Garnier <pascalscn@hotmail.com>
2025-09-09 11:25:57 +0200

    1.3.0: installation de ngx-flags

--- src/app/point-list/point-list.component.ts
+++ src/app/point-list/point-list.component.ts
@@ -28,4 +28,13 @@ export class PointListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     
   }
+
+  getFlag(code: string): string {
+     if (!code) return '';
+  return code
+    .toUpperCase()
+    .replace(/./g, char =>
+      String.fromCodePoint(127397 + char.charCodeAt(0))
+    );
+  }
 }
