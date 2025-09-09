c81edcf6228a7ec08723b4bc979b620212895ab8
Garnier <pascalscn@hotmail.com>
2025-09-09 21:33:17 +0200

    1.3.0: suppression de fichiers appconfig

--- src/app/config/app.config.ts
+++
@@ -1,2 +0,0 @@
-export const AP_URI = 'api/hereiam.php';
-export const MAPBX_ACCESS_TOKEN = "pk.eyJ1IjoiZ2FybmllcnBhc2NhbHdlYiIsImEiOiJjbGticHhjb3cwZ3hzM3J0anhkZHI0MWJkIn0.dn8avAKz_kDLzYchCm5tyw";
--- src/app/point-list/point-list.component.ts
+++ src/app/point-list/point-list.component.ts
@@ -28,13 +28,4 @@ export class PointListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     
   }
-
-  getFlag(code: string): string {
-     if (!code) return '';
-  return code
-    .toUpperCase()
-    .replace(/./g, char =>
-      String.fromCodePoint(127397 + char.charCodeAt(0))
-    );
-  }
 }
