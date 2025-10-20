cad21734a68a1b9327f4b5b1172476b606e47e49
Garnier <pascalscn@hotmail.com>
2025-10-20 18:07:21 +0200

    1.4.0: config: mise en place de la route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigComponent } from './config/config.component';
 import { MarkComponent } from './mark/mark.component';
 import { ShowComponent } from './show/show.component';
 
 const routes: Routes = [ 
   { path : 'show', component : ShowComponent },
   { path : 'mark', component : MarkComponent },
+  { path : 'config', component : ConfigComponent },
   { path : '', component : ShowComponent }
   
 ] ;;
