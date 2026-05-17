b552df27cc5a8e0f6c6f7ce8fc1a816c957725e5
Garnier <pascalscn@hotmail.com>
2026-05-17 13:31:36 +0200

    Remove environment file from tracking

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  mapBoxAccessToken: '',
-  endpoints: {
-    hereiam:'assets/mock/here-i-am-response.json',
-    config:'assets/mock/config-response.json',
-    tracking: 'assets/mock/tracking-response.json'
-  } 
-};
