87080c77cecf7b3c6437d4b6441b13bab0960d68
Garnier <pascalscn@hotmail.com>
2026-05-17 13:31:11 +0200

    1.5.0: suppression du secret dans environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  mapBoxAccessToken: 'pk.eyJ1IjoiZ2FybmllcnBhc2NhbHdlYiIsImEiOiJjbGticHhjb3cwZ3hzM3J0anhkZHI0MWJkIn0.dn8avAKz_kDLzYchCm5tyw',
+  mapBoxAccessToken: '',
   endpoints: {
     hereiam:'assets/mock/here-i-am-response.json',
     config:'assets/mock/config-response.json',
